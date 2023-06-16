55c4f73d83d29eff9e7a64470297291ae36bf0eb
Renan Dev <renan.sp.121@hotmail.com>
2023-06-16 15:58:08 -0300

    [16/06/2023]Parte do codigo de usabilidade refatorado.

--- test driven development/teste_usabilidade.py
+++ test driven development/teste_usabilidade.py
@@ -19,9 +19,6 @@ class BlogTester:
         self.driver.get(url=self.base_url)
         sleep(3.5)
 
-    def find_element(self, by, value):
-        return self.driver.find_element(by, value)
-
     def fill_form(self, elements, values):
         for element, value in zip(elements, values):
             for letter in value:
@@ -29,22 +26,22 @@ class BlogTester:
                 sleep(0.25)
 
     def navegation(self, xpath):
-        nav = self.find_element(By.XPATH, xpath)
+        nav = self.driver.find_element(By.XPATH, xpath)
         sleep(1.75)
         self.driver.execute_script("arguments[0].scrollIntoView();", nav)
         return nav
 
     def perform_login(self, email, password):
-        email_field = self.find_element(By.XPATH, '//*[@id="email"]')
-        password_field = self.find_element(By.XPATH, '//*[@id="password"]')
-        submit_btn = self.find_element(By.XPATH, '//*[@id="submit"]')
+        email_field = self.driver.find_element(By.XPATH, '//*[@id="email"]')
+        password_field = self.driver.find_element(By.XPATH, '//*[@id="password"]')
+        submit_btn = self.driver.find_element(By.XPATH, '//*[@id="submit"]')
         self.driver.execute_script("arguments[0].scrollIntoView();", submit_btn)
         sleep(1.75)
         self.fill_form([email_field, password_field], [email, password])
         sleep(1.25)
         submit_btn.click()
         sleep(1.25)
-        submit_btn = self.find_element(By.XPATH, '//*[@id="submit"]')
+        submit_btn = self.driver.find_element(By.XPATH, '//*[@id="submit"]')
         self.driver.execute_script("arguments[0].scrollIntoView();", submit_btn)
 
     def perform_registration_adm(self, email, password, name):
@@ -56,20 +53,53 @@ class BlogTester:
         self.fill_form([email_entrada, senha_entrada, nome_entrada], [email, password, name])
         submit_btn.click()
 
-    def create_post(self, title, subtitle="Teste de subtitulo",
-                    img_url="https://www.example.com/image.png", content="Olá, CKEditor!"):
-        create_post_btn = self.driver.find_element(By.XPATH, '/html/body/div/div/div/div/a')
-        self.driver.execute_script("arguments[0].scrollIntoView();", create_post_btn)
+    def create_post(self, title, subtitle, img_url, content):
         sleep(3.5)
-        create_post_btn.click()
-        sleep(3.5)
+        titulo_entrada = self.driver.find_element(By.XPATH, '//*[@id="title"]')
+        subtitulo_entrada = self.driver.find_element(By.XPATH, '//*[@id="subtitle"]')
+        url_entrada = self.driver.find_element(By.XPATH, '//*[@id="img_url"]')
+        self.driver.execute_script("arguments[0].scrollIntoView();", titulo_entrada)
+        self.fill_form([titulo_entrada, subtitulo_entrada, url_entrada], [title, subtitle, img_url])
+        sleep(1.25)
+        ckeditor_frame = self.driver.find_element(By.TAG_NAME, "iframe")
+        self.driver.switch_to.frame(ckeditor_frame)
+        body = self.driver.find_element(By.TAG_NAME, "body")
+        body.send_keys(content)
+        self.driver.switch_to.default_content()
+        submit_btn = self.driver.find_element(By.ID, 'submit')
+        sleep(1.25)
+        submit_btn.click()
+
 
     def edit_post(self, new_img_url="https://www.example.com/new_image.png"):
-        # Restante do código omitido por motivos de espaço
+        editar_btn = self.driver.find_element(By.XPATH, '/html/body/article/div/div/div/div[1]/a')
+        self.driver.execute_script("arguments[0].scrollIntoView();", editar_btn)
+        sleep(1)
+        editar_btn.click()
+        url_entrada = self.driver.find_element(By.XPATH, '//*[@id="img_url"]')
+        self.driver.execute_script("arguments[0].scrollIntoView();", url_entrada)
+        sleep(1)
+        url_entrada.clear()
+        sleep(1)
+        url_entrada.send_keys('https://cdn.pixabay.com/photo/2023/06/07/18/14/giraffes-8047856_1280.jpg')
+        sleep(1)
+        submit_btn = self.driver.find_element(By.ID, 'submit')
+        submit_btn.click()
         pass
 
     def post_comment(self, comment="Teste de comentario"):
-        # Restante do código omitido por motivos de espaço
+        ckeditor_frame = self.driver.find_element(By.TAG_NAME, "iframe")
+        self.driver.switch_to.frame(ckeditor_frame)
+        body = self.driver.find_element(By.TAG_NAME, "body")
+        self.driver.execute_script("arguments[0].scrollIntoView();", body)
+        body.send_keys(comment)
+        self.driver.switch_to.default_content()
+        sleep(1)
+        submit_btn = self.driver.find_element(By.ID, 'submit')
+        submit_btn.click()
+        submit_btn = self.driver.find_element(By.ID, 'submit')
+        self.driver.execute_script("arguments[0].scrollIntoView();", submit_btn)
+        sleep(1)
         pass
 
     def visit_about(self):
@@ -95,6 +125,7 @@ class BlogTester:
 
 if __name__ == "__main__":
     tester = BlogTester()
+
     "Tentativa de login de conta que não existe, para mostrar a prevenção de erros."
     sleep(3)
     nav = tester.navegation('//*[@id="navbarResponsive"]/ul/li[2]/a')
@@ -108,13 +139,19 @@ if __name__ == "__main__":
     tester.perform_registration_adm("usuario_adm@teste.com", "123456abc", "Administrador")
 
     "Criar nova postagem"
-    nav = tester.navegation('/html/body/div/div/div/div/a')
-    sleep(1.75)
-    nav.click()
-    for i in range(0,3)
-    tester.create_post("Teste de titulo")
+    routes = ['/html/body/div/div/div/div/a', '/html/body/div/div/div/div[2]/a', '/html/body/div/div/div/div[3]/a']
+    for i in range(0, 3):
+        nav = tester.navegation(routes[i])
+        sleep(1.75)
+        nav.click()
+        sleep(2)
+        tester.create_post(f"Teste de titulo {i+1}", f"Teste de subtitulo {i+1}", "https://www.example.com/image.png", f"Teste conteudo {i+1}")
 
-    # tester.edit_post()
+    "Editar postagem"
+    nav = tester.navegation('/html/body/div/div/div/div[1]/a/h2')
+    sleep(3)
+    nav.click()
+    tester.edit_post()
     # tester.post_comment()
     # tester.visit_about()
     # tester.send_contact_form()
