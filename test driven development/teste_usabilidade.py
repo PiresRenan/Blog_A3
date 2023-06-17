dbad3e3f342940d6b27cff4bcf8558055eb279dd
Renan Dev <renan.sp.121@hotmail.com>
2023-06-17 13:33:25 -0300

    [17/06/2023]Teste de usabilidade terminado.

--- test driven development/teste_usabilidade.py
+++ test driven development/teste_usabilidade.py
@@ -85,39 +85,85 @@ class BlogTester:
         sleep(1)
         submit_btn = self.driver.find_element(By.ID, 'submit')
         submit_btn.click()
-        pass
 
-    def post_comment(self, comment="Teste de comentario"):
+    def post_comment(self, comment):
+        sleep(5)
         ckeditor_frame = self.driver.find_element(By.TAG_NAME, "iframe")
         self.driver.switch_to.frame(ckeditor_frame)
         body = self.driver.find_element(By.TAG_NAME, "body")
         self.driver.execute_script("arguments[0].scrollIntoView();", body)
+        sleep(3)
         body.send_keys(comment)
         self.driver.switch_to.default_content()
-        sleep(1)
+        sleep(3)
         submit_btn = self.driver.find_element(By.ID, 'submit')
         submit_btn.click()
-        submit_btn = self.driver.find_element(By.ID, 'submit')
-        self.driver.execute_script("arguments[0].scrollIntoView();", submit_btn)
-        sleep(1)
-        pass
 
-    def visit_about(self):
-        # Restante do código omitido por motivos de espaço
-        pass
+    def send_contact_form(self, name, email, phone, message):
+        nome_entrada = self.driver.find_element(By.XPATH, '//*[@id="name"]')
+        email_entrada = self.driver.find_element(By.XPATH, '//*[@id="email"]')
+        n_tel_entrada = self.driver.find_element(By.XPATH, '//*[@id="phone"]')
+        mensagem_entrada = self.driver.find_element(By.XPATH, '//*[@id="message"]')
+        contato_enviar_btn = self.driver.find_element(By.XPATH, '//*[@id="sendMessageButton"]')
+        entradas = [nome_entrada, email_entrada, n_tel_entrada, mensagem_entrada]
+        self.driver.execute_script("arguments[0].scrollIntoView();", nome_entrada)
+        self.fill_form(entradas, [name, email, phone, message])
+        sleep(3)
+        contato_enviar_btn.click()
 
-    def send_contact_form(self, name="Teste contato nome", email="Teste contato email", phone="Teste contato telefone",
-                          message="Teste contato mensagem"):
-        # Restante do código omitido por motivos de espaço
-        pass
+    def delete_post(self):
+        sleep(2)
+        delete_post = self.navegation('/html/body/div/div/div/div[3]/p/a')
+        sleep(2)
+        delete_post.click()
+        sleep(2)
+        nav = self.navegation('/html/body/div/div/div/div[2]/p/a')
+
+    def logout(self):
+        nav = self.navegation('//*[@id="navbarResponsive"]/ul/li[2]/a')
+        sleep(2)
+        nav.click()
+
+    def resposividade(self):
+        self.driver.set_window_size(766, 652)
+        nav = self.navegation('//*[@id="mainNav"]/div/button')
+        nav.click()
+
+    def perform_registration_default(self, email, password, name):
+        email_entrada = self.driver.find_element(By.XPATH, '//*[@id="email"]')
+        senha_entrada = self.driver.find_element(By.XPATH, '//*[@id="password"]')
+        nome_entrada = self.driver.find_element(By.XPATH, '//*[@id="name"]')
+        submit_btn = self.driver.find_element(By.XPATH, '//*[@id="submit"]')
+        teste = [email, password, name]
+        entradas = [email_entrada, senha_entrada, nome_entrada]
+        self.fill_form(entradas, teste)
+        submit_btn.click()
+        sleep(3)
+
+    def post_default(self):
+        ckeditor_frame = self.driver.find_element(By.TAG_NAME, "iframe")
+        self.driver.switch_to.frame(ckeditor_frame)
+        body = self.driver.find_element(By.TAG_NAME, "body")
+        self.driver.execute_script("arguments[0].scrollIntoView();", body)
+        body.send_keys("Teste de comentario nao adminstrador.")
+        self.driver.switch_to.default_content()
+        enviar_comentario_btn = self.driver.find_element(By.XPATH, '//*[@id="submit"]')
+        self.driver.execute_script("arguments[0].scrollIntoView();", enviar_comentario_btn)
+        sleep(2)
+        enviar_comentario_btn.click()
+        enviar_comentario_btn = self.driver.find_element(By.XPATH, '//*[@id="submit"]')
+        self.driver.execute_script("arguments[0].scrollIntoView();", enviar_comentario_btn)
+
+    def default_window_size(self):
+        self.driver.maximize_window()
 
     def clear_data(self):
         with app.app_context():
             meta = db.metadata
             for table in reversed(meta.sorted_tables):
-                print('Clear table %s' % table)
                 db.session.execute(table.delete())
             db.session.commit()
+        self.driver.refresh()
 
     def quit(self):
         self.driver.quit()
@@ -126,35 +172,93 @@ class BlogTester:
 if __name__ == "__main__":
     tester = BlogTester()
 
-    "Tentativa de login de conta que não existe, para mostrar a prevenção de erros."
-    sleep(3)
-    nav = tester.navegation('//*[@id="navbarResponsive"]/ul/li[2]/a')
-    nav.click()
-    tester.perform_login("usuario_adm@teste.com", "123456abc")
-    sleep(4)
-    nav = tester.navegation('//*[@id="navbarResponsive"]/ul/li[3]/a')
-    nav.click()
+    while True:
+        "Tentativa de login de conta que não existe, para mostrar a prevenção de erros."
+        sleep(3)
+        nav = tester.navegation('//*[@id="navbarResponsive"]/ul/li[2]/a')
+        nav.click()
+        tester.perform_login("usuario_adm@teste.com", "123456abc")
+        sleep(4)
+        nav = tester.navegation('//*[@id="navbarResponsive"]/ul/li[3]/a')
+        nav.click()
 
-    "Registrar conta administrador."
-    tester.perform_registration_adm("usuario_adm@teste.com", "123456abc", "Administrador")
+        "Registrar conta administrador."
+        tester.perform_registration_adm("usuario_adm@teste.com", "123456abc", "Administrador")
 
-    "Criar nova postagem"
-    routes = ['/html/body/div/div/div/div/a', '/html/body/div/div/div/div[2]/a', '/html/body/div/div/div/div[3]/a']
-    for i in range(0, 3):
-        nav = tester.navegation(routes[i])
-        sleep(1.75)
+        "Criar nova postagem"
+        routes = ['/html/body/div/div/div/div/a', '/html/body/div/div/div/div[2]/a', '/html/body/div/div/div/div[3]/a']
+        for i in range(0, 3):
+            nav = tester.navegation(routes[i])
+            sleep(1.75)
+            nav.click()
+            sleep(2)
+            tester.create_post(f"Teste de titulo {i+1}", f"Teste de subtitulo {i+1}", "https://www.example.com/image.png", f"Teste conteudo {i+1}")
+
+        "Editar postagem"
+        nav = tester.navegation('/html/body/div/div/div/div[1]/a/h2')
+        sleep(3)
+        nav.click()
+        tester.edit_post()
+
+        "Comentar uma postagem"
+        tester.post_comment("Teste de comentario")
+
+        "Teste sobre rota"
+        nav = tester.driver.find_element(By.XPATH, '//*[@id="navbarResponsive"]/ul/li[3]/a')
+        sleep(1)
+        nav.click()
+        tester.navegation('/html/body/div/div/div/p[2]')
+
+        "Teste contato"
+        nav = tester.navegation('//*[@id="navbarResponsive"]/ul/li[4]/a')
+        sleep(2)
+        nav.click()
+        tester.send_contact_form("Teste contato nome", "Teste contato email", "Teste contato telefone", "Teste contato mensagem")
+
+        nav = tester.navegation('//*[@id="navbarResponsive"]/ul/li[1]/a')
+        nav.click()
+
+        "Teste delete"
+        tester.delete_post()
+
+        "Sair da conta"
+        tester.logout()
+
+        "Teste responsividade e conta comum"
+        tester.resposividade()
+
+        "Novo Usuario(Comum)"
+        nav = tester.navegation('//*[@id="navbarResponsive"]/ul/li[3]/a')
+        sleep(2)
+        nav.click()
+        tester.perform_registration_default("deafautl_user@teste.com", "321cba456", "Usuario Comum")
+
+        "Teste de visualização de postagem"
+        nav = tester.navegation('/html/body/div/div/div/div[1]/a/h2')
+        sleep(2)
+        nav.click()
+        tester.navegation('/html/body/article/div/div/div/p')
+        sleep(2)
+
+        "Teste de postagem nao adm"
+        tester.post_default()
+
+        sleep(2)
+        nav = tester.navegation('//*[@id="mainNav"]/div/button')
         nav.click()
         sleep(2)
-        tester.create_post(f"Teste de titulo {i+1}", f"Teste de subtitulo {i+1}", "https://www.example.com/image.png", f"Teste conteudo {i+1}")
+        nav = tester.navegation('//*[@id="navbarResponsive"]/ul/li[1]/a')
+        nav.click()
 
-    "Editar postagem"
-    nav = tester.navegation('/html/body/div/div/div/div[1]/a/h2')
-    sleep(3)
-    nav.click()
-    tester.edit_post()
-    # tester.post_comment()
-    # tester.visit_about()
-    # tester.send_contact_form()
-    tester.clear_data()
-    sleep(600)
-    tester.quit()
+        sleep(2)
+        tester.navegation('/html/body/div/div/div/div[2]/a/h3')
+        sleep(3)
+        tester.navegation('/html/body/header/div[2]/div/div/div/h1')
+        sleep(3)
+
+        "Retornar ao tamanho total de janela"
+        tester.default_window_size()
+
+        tester.clear_data()
+        sleep(3)
+
